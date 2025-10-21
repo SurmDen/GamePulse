7eb37deac2b309a533a7749263715a3bc32b1ffa
Denis Surmanidze <surmanidzedenis609@gmail.com>
2025-10-21 09:58:00 +0300

    added core layer

---
+++ GamePulse.Core/Entites/GameInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamePulse.Core.Entites
+{
+    public class GameInfo : EntityBase
+    {
+        public string GameName { get; set; } = string.Empty;
+
+        public DateTime DateOfRelease { get; set; }
+
+        public string ShopRef { get; set; } = string.Empty;
+
+        public string ImageRef { get; set; } = string.Empty;
+    }
+}

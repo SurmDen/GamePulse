c0cb7ba3904a8402b77b71271252e3c1c8795535
Denis Surmanidze <surmanidzedenis609@gmail.com>
2025-10-23 13:43:21 +0300

    added repos to application layer

---
+++ GamePulse.Core/Interfaces/Repositories/IGameRepository.cs
@@ -0,0 +1,16 @@
+﻿using GamePulse.Core.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamePulse.Core.Interfaces.Repositories
+{
+    public interface IGameRepository
+    {
+        public Task AddGamesAsync(List<Game> games);
+
+        public Task<List<Game>> GetGamesAsync(int month, long? tagId = null, string? platform = null);
+    }
+}
---
+++ GamePulse.Core/Interfaces/Repositories/IGenreRepository.cs
@@ -0,0 +1,14 @@
+﻿using GamePulse.Core.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamePulse.Core.Interfaces.Repositories
+{
+    public interface IGenreRepository
+    {
+        public Task<List<Genre>> GetTopGenresWithGamesAsync(int genresCount);
+    }
+}
